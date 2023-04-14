5697f467f0f2958cd2242704195d33ff29d90f62
yosachan <127594209+yosachan@users.noreply.github.com>
2023-04-15 02:24:44 +0900

    Add files via upload

---
+++ jk_c_002.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main (void){
+    // printf("こんにちは\n");
+    // printf("ありがとう\n");
+
+    // printf("%d", 100);
+
+    // printf("%d", 100);
+    // printf("円\n");
+    // printf("%d円", 200);
+
+    // printf("%d+", 100);
+    // printf("%d=", 200);
+    // printf("%d\n", 300);
+    // printf("%d+%d=%d\n", 100,200,300);
+    // printf("合計は、%d円です。", 300);
+
+    // printf("%d\n", 10+3);
+    // printf("%d\n", 10-3);
+    // printf("%d\n", 10*3);
+    // printf("%d\n", 10/3);
+    // printf("%d\n", 10+3*6-7);
+
+    printf("%f\n", 3.14);
+    printf("%f\n", 3.14*3*3);
+
+
+
+    return 0;
+}
