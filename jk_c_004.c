fa8a34bc6a28f10110c3083aed23c0314467ef5d
yosachan <127594209+yosachan@users.noreply.github.com>
2023-04-17 01:51:16 +0900

    Add files via upload

---
+++ jk_c_004.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+int main (void){
+    // int apple;
+    // int orange;
+    // apple = 5;
+    // orange = 10;
+    // int apple = 5;
+    // int orange = 10;
+    int apple = 10;
+    // apple += 5;
+    // printf("%d\n", apple);
+    // apple -= 5;
+    // printf("%d\n", apple);
+    // apple *= 5;
+    // printf("%d\n", apple);
+    // apple /= 5;
+    // apple++;
+    // printf("%d\n", apple);
+    // apple++;
+    // printf("%d\n", apple);
+    // apple++;
+    // printf("%d\n", apple);
+    // apple--;
+    // printf("%d\n", apple);
+    // apple--;
+    // printf("%d\n", apple);
+    // printf("%f", 3.14*3*3);
+    // printf("%d", (int)3.14);
+    // printf("%d", (int) (3.14*3*3));
+    // int suuti;
+    // scanf("%d", &suuti);
+    // printf("入力された値は %d です。", suuti);
+    double suuti;
+    scanf("%lf", &suuti);
+    printf("入力された値は %lf です。", suuti);
+
+
+
+
+
+
+
+    return 0;
+}
